eb63cad40e1defced5820d6aad60756e26096157
Patton <pattontanges@csu.fullerton.edu>
2024-10-19 14:52:15 -0700

    Initial set up for simple static HTML front web page & routing

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__) # WSGI class directs Flask resources
+
+@app.route('/') # Decorator, called by a given URL
+def index():
+	return render_template('index.html')
