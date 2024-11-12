d6edd9e84ceffa00c6d6a22da0fcc4888c1a8ec4
Patton <pattontanges@csu.fullerton.edu>
2024-11-11 22:22:26 -0800

    Canvas dynamically resizes when window changes size. Also retains draw data.

--- static/js/whiteboard.js
+++ static/js/whiteboard.js
@@ -4,34 +4,30 @@
 let canvas = document.getElementById('whiteboard');
 let context = canvas.getContext('2d');
 
-context.width = window.innerWidth;
-context.height = window.innerHeight;
-
-canvas.height = document.getElementsByClassName('main')[0].clientHeight;
-canvas.width = document.getElementsByClassName('main')[0].clientWidth;
-
 let isMouseDrawing = false;
 
-/* Draw Border */
-/* To Do: Update canvas dimensions & size with event resize */
-context.strokeStyle = 'white';
-context.lineWidth = '10';
-context.strokeRect(0, 0, document.getElementsByClassName('main')[0].clientWidth, document.getElementsByClassName('main')[0].clientHeight-10);
+function initialize(){
+    canvas.height = document.getElementsByClassName('main')[0].clientHeight;
+    canvas.width = document.getElementsByClassName('main')[0].clientWidth;
 
-/* Canvas Drawing Functionality */
-canvas.addEventListener('mousedown', (event) =>{
-    isMouseDrawing = true;
-    context.moveTo(event.offsetX, event.offsetY);
-});
+    canvas.addEventListener('mousedown', (event) =>{
+        isMouseDrawing = true;
+        context.moveTo(event.offsetX, event.offsetY);
+    });
 
-canvas.addEventListener('mouseup', (event) =>{
-    isMouseDrawing = false;
-    context.beginPath();
-});
+    canvas.addEventListener('mouseup', (event) =>{
+        isMouseDrawing = false;
+        context.beginPath();
+    });
 
-canvas.addEventListener('mousemove', update);
+    canvas.addEventListener('mousemove', draw_update);
 
-function update(event){
+    console.log("HERE")
+    window.addEventListener('resize', resize_canvas, false);
+}
+
+
+function draw_update(event){
     if (isMouseDrawing) {
         context.lineWidth = 5; /* Pen Size */
         context.lineCap = "round"; /* Pen Type */
@@ -39,6 +35,22 @@ function update(event){
         context.lineTo(event.offsetX, event.offsetY);
         context.stroke();
     } /* end if */
+}
+
+function resize_canvas(event){
+    /* Save existing Canvas */
+    const data_url = canvas.toDataURL();
+
+    /* Resize Canvas, note: context.h/w automatically get updated */
+    canvas.height = document.getElementsByClassName('main')[0].clientHeight;
+    canvas.width = context.width = document.getElementsByClassName('main')[0].clientWidth;
+
+    /* Apply saved image to new Canvas */
+    const image = new Image();
+    image.src = data_url;
+    image.onload = () => {
+        context.drawImage(image, 0, 0);
+    };
 }
 
 /* Button Commands */
@@ -55,3 +67,8 @@ function randomizeBackground(x, y, width, height, color) {
     context.fillRect(x, y, width, height, color);
 }
 */
+
+document.addEventListener("DOMContentLoaded", initialize, {once : true});
+document.addEventListener("DOMContentLoaded", () => {
+    resize_canvas();
+});
