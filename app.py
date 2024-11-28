9f3a4d260f39a0f890f5248eaf873ba67977dfd3
Patton <pattontanges@csu.fullerton.edu>
2024-11-27 22:00:59 -0800

    ✨ WebSockets implementation. Client connects to server and may send msgs

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 # Canvas Back-End Logic
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, leave_room, send, emit, ConnectionRefusedError
+import datetime
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+user_connection_counter = 0;
 
-default_connect_message = "User connected!"
-default_disconnect_message = "User disconnected!"
-
-@app.route('/')
+@app.route('/') # Decorator, called by a given URL
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@socketio.on('message_event')
-def message_event(data):
-    emit('response_event', {'message':data}, broadcast=True)
+@socketio.on('message') # SocketIO messages are received by both parties as events, JS callbacks on client-side
+def handle_message(data):
+    date = datetime.datetime.now()
+    print(f'{date} - Received Message: {data}')
 
-@socketio.on('connect')
-def on_connect():
-    emit('response_event', {'message': default_connect_message}, broadcast=True)
+@socketio.on('connect') # On client connection to server
+def connect():
+    print("Client connected!")
+    print("SID: " + request.sid)
+    global user_connection_counter # Global allows for variable to be modified
+    user_connection_counter += 1
+    print("User Connection Counter: " + str(user_connection_counter))
 
-@socketio.on('disconnect')
-def on_disconnect():
-    emit('response_event', {'message': default_disconnect_message}, broadcast=True)
+
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app) # encapsulates & replaces app.run() for websocket support
