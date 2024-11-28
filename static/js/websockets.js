0711718061bef4dad99fea67a20e4b5d42f3683c
Patton <pattontanges@csu.fullerton.edu>
2024-11-27 22:00:53 -0800

    ✨ WebSockets implementation. Client connects to server and may send msgs

--- static/js/websockets.js
+++ static/js/websockets.js
@@ -1,13 +1,6 @@
-const socket = io.connect('http://127.0.0.1:5000');
+socket = io();
+socket.connect('http://127.0.0.1:5000/');
 
-document.getElementById("clear_canvas").addEventListener("click", push_message);
-
-
-socket.on('response_event', (data)=>{
-    document.getElementById("web-message").innerHTML = data.message;
+socket.on('connect', function(){
+    socket.send("New user connected!");
 });
-
-function push_message(){
-    socket.emit('message_event', { 'message': "outgoing-message" });
-    // Also may use socket.send() for standard...
-}
